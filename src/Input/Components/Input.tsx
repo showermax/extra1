eadcde2cbae3ac82f6fd81ba47f8511a0f30c3cf
showermax <sevenhauz@gmail.com>
2023-03-11 12:59:12 +0300

    input onchange

---
+++ src/Input/Components/Input.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export function Input() {
+    
+    return (
+        <div>
+        <input />
+        <button> Add </button>
+        </div>
+    )
+}
