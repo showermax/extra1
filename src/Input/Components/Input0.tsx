dc088a770efe314958b0bb037172e23d96d8f101
showermax <sevenhauz@gmail.com>
2023-03-11 16:05:53 +0300

    divided button and input

---
+++ src/Input/Components/Input0.tsx
@@ -0,0 +1,18 @@
+import React, { ChangeEvent, useState } from 'react';
+
+
+type Input0PropsType = {
+    setMessprops: (mess: string)=> void
+    messprops: string
+}
+
+export function Input0(props:Input0PropsType) {
+    const onchangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        props.setMessprops(event.currentTarget.value)
+    }
+    return (
+        <div>
+            <input onChange = {onchangeInputHandler} value={props.messprops} />
+        </div>
+    )
+}
