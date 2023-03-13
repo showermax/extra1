fbb5d63e9bc27fd048a28e2eb5a63e83584ae1cf
showermax <sevenhauz@gmail.com>
2023-03-13 22:16:56 +0300

    Extra with tuesday

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import { InputLesson } from './Input/InputLesson';
+import Navbar from "./Components/Navbar"
+import Main from "./Components/Main"
+
 
 
 function App() {
-  return (
-    <div className="App">
-      <InputLesson/>
-      <hr/>
-    </div>
+const [theme, setTheme] = useState (false)
+
+const toggleDarkMode = (isDark: boolean) => {
+        setTheme(isDark)
+}
+    return (
+        <div className='container'>
+            <Navbar darkMode = {theme} toggleDarkMode = {toggleDarkMode}/>
+            <Main darkMode = {theme} />
+        </div>
   );
 }
 
 export default App;
+
+
+// Пн - Нативка Карина Кришнева, Илья Матюшин
+// Вт - ВЫХОДНОЙ
+// Ср - ВЫХОДНОЙ
+// Чт - СоцСеть Юля Слатвицкая, Костя Сильченко
+// Пт - Тудулист Илья Шулапов, Денис Полуносик
+// Сб – 11:00 Верстка
+// Вс - Тудулист Саня Юдинцев, Рома Шинкаренко (гости Вася, Артем, Володя)
+
--- src/Input/Components/Messages.tsx
+++ src/Input/Components/Messages.tsx
@@ -3,10 +3,9 @@ type MessagesPropsType = {
     newmessages: Array<string>
 }
 export function Messages(props: MessagesPropsType) {
-    console.log(props.newmessages)
     return (
         <>
-        {props.newmessages.map((el,i)=><div key={i}>{el}</div>) }
+            {props.newmessages.map((el, i) => <div key={i}>{el}</div>)}
         </>
     )
 }
